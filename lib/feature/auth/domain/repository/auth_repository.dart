a44fe1acaa44452375aa096d9be4533f2064d217
praam <praa.m.2002@gmail.com>
2023-07-05 12:17:11 +0300

    updates to mack the previous commit is stable because some problems in auth_repository.dart

--- lib/feature/auth/domain/repository/auth_repository.dart
+++ lib/feature/auth/domain/repository/auth_repository.dart
@@ -1,12 +1,13 @@
 import 'package:dartz/dartz.dart';
+import 'package:uncle_bob/core/error/failure.dart';
 import 'package:uncle_bob/feature/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, User>> login();
+  Future<Either<Failure, User>> login(String email, String password);
 
-  Future<Either<Failure, User>> signup();
+  Future<Either<Failure, User>> signup(User user);
 
-  Future<Either<Failure, Unit>> forgotPassword();
+  Future<Either<Failure, Unit>> forgotPassword(String code, String newPassword);
 
-  Future<Either<Failure, Unit>> deleteUser();
+  Future<Either<Failure, Unit>> deleteUser(String userToken);
 }
